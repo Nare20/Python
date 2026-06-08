6418293300f0d1298f979c3cb0e5f991b70e46d3
Nare Grigoryan <naregrigoryan15@gmail.com>
2026-06-08 17:55:50 +0400

    rock-paper-scissors game

--- first_project/guess_numbers.py
+++ first_project/guess_numbers.py
@@ -1,45 +1,45 @@
-# import random
+import random
 
-# number_guess = random.randint(1,100)
-# while True:
-#     try:
-#         guess = int(input("Guess the number between 1 and 100: "))
+number_guess = random.randint(1,100)
+while True:
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
 
-#         if guess < number_guess:
-#             print("Too low")
-#         elif guess > number_guess: 
-#             print("Too high! ")
-#         else: 
-#             print("Congratulation you are guess!! ")
-#             break
-#     except ValueError:
-#         print("please enter a valid number")
+        if guess < number_guess:
+            print("Too low")
+        elif guess > number_guess: 
+            print("Too high! ")
+        else: 
+            print("Congratulation you are guess!! ")
+            break
+    except ValueError:
+        print("please enter a valid number")
 
-# #import random
-# emojic = {'r':'🪨', 's':'✂️', 'p': '📄'}
-# choices = ('r', 'p','s')
-# while True:
-#     choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
-#     if choice not in choices:
-#         print('invalid choice!')
-#         continue
+#import random
+emojic = {'r':'🪨', 's':'✂️', 'p': '📄'}
+choices = ('r', 'p','s')
+while True:
+    choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
+    if choice not in choices:
+        print('invalid choice!')
+        continue
 
-#     computer_choice = random.choice(choices)
+    computer_choice = random.choice(choices)
 
-#     print(f' You chose {emojic[choice]} ')
-#     print(f' Computer_choice {emojic[computer_choice]}')
+    print(f' You chose {emojic[choice]} ')
+    print(f' Computer_choice {emojic[computer_choice]}')
 
-#     if choice == computer_choice:
-#         print('Tie!')
-#     elif (
-#         (choice == 'r' and computer_choice == 's') or 
-#         (choice == 's' and computer_choice == 'p') or 
-#         (choice == 'p' and computer_choice == 'r')):
-#         print("you win" )
-#      else:
-#         print('you lose! ' )
-#     ask_quetion = input('continue? (y/n):').lower()
-#     if ask_quetion == 'n':
+    if choice == computer_choice:
+        print('Tie!')
+    elif (
+        (choice == 'r' and computer_choice == 's') or 
+        (choice == 's' and computer_choice == 'p') or 
+        (choice == 'p' and computer_choice == 'r')):
+        print("you win" )
+     else:
+        print('you lose! ' )
+    ask_quetion = input('continue? (y/n):').lower()
+    if ask_quetion == 'n':
         
 
         
